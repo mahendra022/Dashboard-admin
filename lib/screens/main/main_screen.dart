6a236a3888a43dfe7e471af5acebc05fdae341ea
mahendra022 <ekamahendra86@gmail.com>
2022-06-01 14:24:03 +0700

    Add folder assets and all filed needed

---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../responsive.dart';
+import 'components/slide_menu.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const SideMenu(),
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              const Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
+            Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: Text('Hai'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
